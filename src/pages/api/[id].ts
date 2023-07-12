6571cf956328d439bf5455137f3542ed3c805e95
AhmedRamiBoukef <ahmedramiboukef@gmail.com>
2023-07-12 18:19:49 +0100

    Fixing deployment errors

--- src/pages/api/[id].ts
+++ src/pages/api/[id].ts
@@ -2,34 +2,35 @@ import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../prisma/client";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const id = req.query.id as string;
   try {
     if (req.method === "GET") {
       const data = await prisma.post.findFirst({
         where: {
-            id: req.query.id
-        } 
+          id,
+        },
       });
       res.status(200).json(data);
     } else if (req.method === "PUT") {
       const body = JSON.parse(req.body);
       const data = await prisma.post.update({
         where: {
-            id: body.id
-          },
+          id,
+        },
         data: { title: body.title, body: body.body },
       });
       res.status(200).json(data);
     } else if (req.method === "DELETE") {
       const data = await prisma.post.delete({
         where: {
-            id: req.query.id
-          }
+          id,
+        },
       });
       res.status(200).json(data);
     }
   } catch (e) {
     console.log(e);
-    
+
     res.status(500).json(e);
   }
 };
